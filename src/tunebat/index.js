0b817449f544e5ccb5eb2d6f6efe820121c343bb
oscar.di-lenarda <oscar.di-lenarda@davidson.fr>
2024-08-01 13:46:45 +0200

    fix(tunebat): improve tunebat precise search with dash suffixes

--- src/tunebat/index.js
+++ src/tunebat/index.js
@@ -4,7 +4,7 @@ const cleanWordsFromTrackName = (trackName) => {
   let wordsToRemove = ['feat.', 'feat', 'ft.', 'ft', 'with', 'w/'];
   wordsToRemove.map((word) => wordsToRemove.push(`(${word}`, `[${word}`));
 
-  const words = trackName.split(' ');
+  const words = trackName.toLowerCase().split(' ');
 
   // find the index of the word to remove
   const index = words.findIndex((word) => wordsToRemove.includes(word));
@@ -17,20 +17,22 @@ const cleanWordsFromTrackName = (trackName) => {
     case '(':
       // remove what's inside the parentheses
       const closingP = words.findIndex((word) => word.endsWith(')'));
-      return words.slice(0, index).join(' ') + words.slice(closingP + 1).join(' ');
+      return [words.slice(0, index).join(' '), words.slice(closingP + 1).join(' ')].join(' ').trim();
     case '[':
       // remove what's inside the brackets
       const closingB = words.findIndex((word) => word.endsWith(']'));
-      return words.slice(0, index).join(' ') + words.slice(closingB + 1).join(' ');
+      return [words.slice(0, index).join(' '), words.slice(closingB + 1).join(' ')].join(' ').trim();
     default:
-      return words.slice(0, index).join(' ') + words.slice(index + 1).join(' ');
+      const closing = words.findIndex((word) => word === '-');
+      const suffix = words.slice(closing).join(' '); // If there's a dash for a remix, keep it
+      return [words.slice(0, index).join(' '), closing > -1 ? ` ${suffix}` : ''].join(' ').trim();
   }
 };
 
 export const getTunebatTrack = async (command, searchTerm, spotifyTrackName) => {
   const attemptSearch = async () => {
     const sanitizedSearchTerm = searchTerm.map((term) => term.replace(/[;&|()]/g, ''));
-    const res = await fetch(`https://api.tunebat.com/api/tracks/search?term=${sanitizedSearchTerm.join('%20')}`);
+    const res = await fetch(`https://api.tunebat.com/api/tracks/search?term=${sanitizedSearchTerm.join(' ')}`);
 
     switch (res.status) {
       case 200:
